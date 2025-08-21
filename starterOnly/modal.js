77a5d60aae940a0e23e6c8cf46acde68e30f783c
Yonas <younesbeji55@gmail.com>
2025-08-21 14:41:38 +0200

    Resolution Fermeture Pop-up

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,16 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const formulaire = document.getElementById("formulaire");
+const FirstNameErreur = document.querySelector(".FirstNameErreur");
+const LastNameErreur = document.querySelector(".LastNameErreur");
+const EmailErreur = document.querySelector(".EmailErreur");
+const CheckboxErreur = document.querySelector(".Check-boxErreur");
+
+const firstname = document.getElementById('first');
+const lastname = document.getElementById('last');
+const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const cities = document.querySelectorAll('[name="location"]')
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -29,3 +39,39 @@ if (closePopupBtn) { // Verifie si le bouton existe
     modalbg.style.display = "none"; // Ferme la pop-up
   });
 }
+
+// pour valider l'entrée du prenom si deux caractères ou plus
+function validate() {
+  if (firstname.value.length < 2) {
+    FirstNameErreur.style.display = "block";
+  } else {
+    FirstNameErreur.style.display = "none";
+  }
+
+  // pour valider l'entrée du nom si deux caractères ou plus
+  if (lastname.value.length < 2) {
+    LastNameErreur.style.display = "block";
+  } else {
+    LastNameErreur.style.display = "none";
+  }
+
+  //Pour que les entrées de l'email soit valides
+  if (!emailRegex.test(email.value)) {
+    EmailErreur.style.display = "block";
+  } else {
+    EmailErreur.style.display = "none";
+  }
+
+  //
+  CheckboxErreur.style.display ="block";
+  cities.forEach ((city) => {
+    if (city.checked) {
+      CheckboxErreur.style.display ="none";
+    }
+  })
+}
+
+formulaire.addEventListener('submit', (e) => {
+  e.preventDefault()
+  validate()
+})
